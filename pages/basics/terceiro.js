660167ab18c270a7487b9f3e0fdffa493c09a5b5
je-almeida <jeff.ifsp@gmail.com>
2021-08-26 00:34:26 -0300

    set key links

--- pages/basics/terceiro.js
+++ pages/basics/terceiro.js
@@ -1,9 +1,9 @@
 export default function Jsx() {
 
     const array = [
-        <li>X1</li>,
-        <li>X2</li>,
-        <li>X3</li>
+        <li key={1}>X1</li>,
+        <li key={2}>X2</li>,
+        <li key={3}>X3</li>
     ]
 
     const content = (
